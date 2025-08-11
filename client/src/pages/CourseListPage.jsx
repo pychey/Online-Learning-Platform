aa313147c6b5e68a61a219ae7a9345edea6a8b20
pychey <prakpycheyy@gmail.com>
2025-08-12 00:42:19 +0700

    Implement Course Management & Course Recommend

--- client/src/pages/CourseListPage.jsx
+++ client/src/pages/CourseListPage.jsx
@@ -1,11 +1,15 @@
+import CourseManagement from "../components/CourseManagement"
 
 const CourseListPage = () => {
 
   return(
-    <section className="mt-20 w-full bg-amber-300 ">
-      
-    </section>
+    <>
+      <section className="mt-20 w-full bg-amber-300 ">
+        
+      </section>
+      <CourseManagement />
+    </>
   )
 }
 
-export default CourseListPage
+export default CourseListPage;
