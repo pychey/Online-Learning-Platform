07ad147b17d1c652eb282de38ee69cb66a104ccd
leap <Bunleab.sok@student.cadt.edu.kh>
2025-08-14 13:38:43 +0700

    LoginAndRegisterPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,9 @@ import Footer from "./components/Footer";
 import RegisterLoginPage from "./pages/RegisterLoginPage";
 import Cart from "./pages/Cart";
 import Institute from "./pages/Institute";
+import LostPasswordPage from "./pages/LostPasswordPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
 
@@ -22,8 +25,10 @@ function App() {
         <Route path="/institute" element={<Institute />} />
         <Route path="/verify" element={<VerifyPage />} />
         <Route path="/my-account-settings" element={<RegisterLoginPage/>} />
+        <Route path="/lost-password"element={<LostPasswordPage />} />
+        <Route path="/login"element={<LoginPage />} />
+        <Route path="/register"element={<RegisterPage />} />
       </Routes>
-
       <Footer/>
     </BrowserRouter>
   )
