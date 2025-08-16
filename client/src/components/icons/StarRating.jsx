d35ce8aae7253e3ca355fefcb673c5b5514f93c5
pychey <prakpycheyy@gmail.com>
2025-08-17 00:34:01 +0700

    Implement Program Courses

--- client/src/components/icons/StarRating.jsx
+++ client/src/components/icons/StarRating.jsx
@@ -1,6 +1,6 @@
 import { khmerToEnglishNumber } from "../../utils/khmerToEnglishNumber.js";
 
-const StarRating = ({ ratingNumber = true ,rating, className = 'flex text-base select-none' }) => {
+const StarRating = ({ ratingNumber = true ,rating, className = 'flex text-base select-none', outline = true }) => {
   const englishRating = khmerToEnglishNumber(rating);
 
   return (
@@ -13,7 +13,7 @@ const StarRating = ({ ratingNumber = true ,rating, className = 'flex text-base s
           <span
             key={i}
             className="relative inline-block text-white"
-            style={{ WebkitTextStroke: "1px #facc15" }}
+            style={ outline ? { WebkitTextStroke: "1px #facc15" } : { }}
           >
             <span
               className={`absolute top-0 left-0 overflow-hidden text-yellow-400 pointer-events-none`}
--- client/src/pages/ProgramDetail.jsx
+++ client/src/pages/ProgramDetail.jsx
@@ -1,4 +1,6 @@
+import ProgramCourses from "../components/ProgramCourses.jsx";
 import ProgramOverview from "../components/ProgramOverview.jsx";
+import PROGRAM_COURSES_CONTENT from "../data/program_courses.js";
 import PROGRAM_OVERVIEW from "../data/program_overview.js";
 
 
@@ -6,6 +8,7 @@ function ProgramDetail (){
     return (
         <>
            <ProgramOverview data={PROGRAM_OVERVIEW}/>
+           <ProgramCourses courses={PROGRAM_COURSES_CONTENT}/>
         </>
     )
 }
