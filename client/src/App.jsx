6c7055de94e04fad4971507746e58b2bf3cac121
pychey <prakpycheyy@gmail.com>
2025-08-17 14:26:45 +0700

    Add Guide Page For Easy Access to All Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,6 @@ import NavBar from "./components/navBar";
 import Footer from "./components/Footer";
 import RegisterLoginPage from "./pages/RegisterLoginPage";
 import Help from "./pages/Help"
-
 import Cart from "./pages/Cart";
 import Institute from "./pages/Institute";
 import ProgramDetail from "./pages/ProgramDetail";
@@ -17,6 +16,7 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import MyCoursePage from "./pages/MyCoursePage";
 import MyCertificatePage from "./pages/MyCertificatePage";
+import GuidePage from "./pages/GuidePage";
 
 function App() {
 
@@ -25,6 +25,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/guide" element={<GuidePage />}/>
         <Route path="/courselist" element={<CourseListPage />} />
         <Route path="/product/:id" element={<CourseDetailPage />} />
         <Route path="/program/:id" element={<ProgramDetail />} />
