168419f12b10dee3cdac4ed8fb89d36d0fa413d7
pychey <prakpycheyy@gmail.com>
2025-08-18 16:06:19 +0700

    Implement DownloadCertificate Component

--- client/src/pages/MyCertificatePage.jsx
+++ client/src/pages/MyCertificatePage.jsx
@@ -1,11 +1,12 @@
 import DashboardHeader from "../components/DashboardHeader"
+import DownloadCertificate from "../components/DownloadCertificate"
 
 const MyCertificatePage = () => {
 
   return(
     <main>
       <DashboardHeader />
-      
+      <DownloadCertificate />
     </main>
   )
 }
