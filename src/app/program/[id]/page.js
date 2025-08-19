df6efc948cf944b652666743a8ab68a039f2f836
pychey <prakpycheyy@gmail.com>
2025-08-19 14:31:47 +0700

    Finish Other Program Certificate

--- src/app/program/[id]/page.js
+++ src/app/program/[id]/page.js
@@ -1,6 +1,7 @@
 import ProgramOverview from "@/components/ProgramDetailPage/ProgramOverview"
 import ProgramCourses from "@/components/ProgramDetailPage/ProgramCourses"
 import KeyBenefits from "@/components/ProgramDetailPage/KeyBenefits"
+import OtherProgramCertificate from "@/components/ProgramDetailPage/OtherProgramCertificate"
 
 import PROGRAM_OVERVIEW from "@/data/program_overview"
 import PROGRAM_COURSES_CONTENT from "@/data/program_courses"
@@ -11,6 +12,7 @@ const ProgramDetailPage = () => {
       <ProgramOverview data={PROGRAM_OVERVIEW}/>
       <ProgramCourses courses={PROGRAM_COURSES_CONTENT}/>
       <KeyBenefits />
+      <OtherProgramCertificate />
     </main>
   )
 }
