d173d2044d22eef66bf9d3d2b411558872efc17b
sokundeny <sokundeny18@gmail.com>
2025-08-19 14:20:08 +0700

    convert a to link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -43,18 +43,23 @@ const NavBar = () => {
     setOpenMenu(prev=>!prev)
   }
 
+  const handleLinkClick = () => {
+    setOpenMenu(false);
+    setActiveSubmenuIndex(null); // optional: close any open submenu
+  }
+
   return (
     <nav className='w-full h-20 border-gray shadow-md fixed top-0 left-0 right-0 bg-white z-50'>
       <div className='flex items-center justify-between max-w-[1100px] mx-auto px-6 tablet:px-2'>
         <div className="w-[45%] tablet:w-60 h-full ">
-          <a className="w-full h-full" href={page.guide}><img src="/Logo-AA-Horizontal.png" alt="logo" className="h-full w-full object-cover"/></a>
+          <Link className="w-full h-full" href={page.guide}><img src="/Logo-AA-Horizontal.png" alt="logo" className="h-full w-full object-cover"/></Link>
         </div>
         <div className="hidden laptop:flex items-center justify-end gap-2.5 lg:gap-4 xl:gap-6 py-5 ">
             {nav.map((n, index) => (
             <div key={index} className="relative group">
               <div className="text-gray-800 font-[600] hover:text-primary-color cursor-pointer py-2 px-1 select-none group-hover:text-gray-400">
                 <span className="inline-flex items-center gap-1">
-                  <a href={n.link}>{n.title}</a>
+                  <Link href={n.link}>{n.title}</Link>
                   {n.submenu && <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6l-6-6z"/></svg>}
                 </span>
               </div>
@@ -68,13 +73,13 @@ const NavBar = () => {
                 </div>
 
                 {n.submenu.map((sub, subIndex) => (
-                  <a
+                  <Link
                     key={subIndex}
                     href={sub.link}
                     className="px-4 py-2 text-sm font-semibold text-gray-400 hover:text-primary whitespace-nowrap block hover:bg-gray-200"
                   >
                     {sub.title}
-                  </a>
+                  </Link>
                 ))}
               </div>
             )}
@@ -94,7 +99,7 @@ const NavBar = () => {
           </a>
         </div>
         <div className="flex laptop:hidden items-center justify-end tablet:gap-6 gap-6 py-5 ">
-          <a className="h-10 w-10 bg-primary flex items-center justify-center rounded-lg"href={page.cart}>
+          <Link className="h-10 w-10 bg-primary flex items-center justify-center rounded-lg"href={page.cart}>
             <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 24 24"
@@ -104,8 +109,8 @@ const NavBar = () => {
                       strokeWidth="0.9">
               <path d="M6.616 21q-.691 0-1.153-.462T5 19.385V8.615q0-.69.463-1.152T6.616 7H8.5v-.5q0-1.458 1.021-2.479T12 3t2.479 1.021T15.5 6.5V7h1.885q.69 0 1.152.463T19 8.616v10.769q0 .69-.463 1.153T17.385 21zm0-1h10.769q.23 0 .423-.192t.192-.424V8.616q0-.231-.192-.424T17.384 8H15.5v2.5q0 .214-.143.357T15 11t-.357-.143t-.143-.357V8h-5v2.5q0 .214-.143.357T9 11t-.357-.143T8.5 10.5V8H6.616q-.231 0-.424.192T6 8.616v10.769q0 .23.192.423t.423.192M9.5 7h5v-.5q0-1.056-.722-1.778T12 4t-1.778.722T9.5 6.5zM6 20V8z" />
             </svg>
-          </a>
-          <a className="h-10 w-10 bg-primary flex items-center justify-center rounded-lg"href={page.login}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className='h-8 w-8 text-white'><path fill="currentColor" d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4s-4 1.79-4 4s1.79 4 4 4m0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4" /></svg></a>
+          </Link>
+          <Link className="h-10 w-10 bg-primary flex items-center justify-center rounded-lg"href={page.login}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className='h-8 w-8 text-white'><path fill="currentColor" d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4s-4 1.79-4 4s1.79 4 4 4m0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4" /></svg></Link>
           <button className={`h-10 tablet:w-auto w-10 bg-primary flex items-center justify-center rounded-lg`} onClick={handleMenuClick}><span className='flex px-4 items-center justify-center gap-4'><svg xmlns="http://www.w3.org/2000/svg" className='h-7 w-7 text-white' viewBox="0 0 24 24"><path fill="currentColor" d="M4 18q-.425 0-.712-.288T3 17t.288-.712T4 16h16q.425 0 .713.288T21 17t-.288.713T20 18zm0-5q-.425 0-.712-.288T3 12t.288-.712T4 11h16q.425 0 .713.288T21 12t-.288.713T20 13zm0-5q-.425 0-.712-.288T3 7t.288-.712T4 6h16q.425 0 .713.288T21 7t-.288.713T20 8z"/></svg><p className='text-white tablet:block hidden'>មីនុយ</p></span></button>
         </div>
         {openMenu && (
@@ -122,9 +127,10 @@ const NavBar = () => {
               {nav.map((n, index) => (
                 <div key={index} className="border-b border-gray-300">
                   <div className="w-full text-left py-4 pl-4 pr-4 font-semibold text-gray-800 flex items-center justify-between">
-                  <a href={n.link}>
+                  <Link href={n.link}
+                  onClick={handleLinkClick}>
                     {n.title}
-                  </a>
+                  </Link>
                   <button
                     className=""
                     onClick={() => toggleSubmenu(index)}
@@ -138,19 +144,20 @@ const NavBar = () => {
                   {n.submenu && (
                     <div className={`bg-gray-50 overflow-hidden transform transition-transform duration-1000 ease-out ${activeSubmenuIndex === index?"max-h-40":"max-h-0"}`}>
                       {n.submenu.map((sub, subIndex) => (
-                        <a
+                        <Link
                           key={subIndex}
                           href={sub.link}
                           className="block py-2 pl-8 text-sm text-gray-600 hover:bg-gray-100"
+                          onClick={handleLinkClick}
                         >
                           {sub.title}
-                        </a>
+                        </Link>
                       ))}
                     </div>
                   )}
                 </div>
               ))}
-              <a href={page.login} className='bg-primary text-white py-2 px-2 '>ចូលរៀន/ចុះឈ្មោះ</a>
+              <Link href={page.login} className='bg-primary text-white py-2 px-2 '>ចូលរៀន/ចុះឈ្មោះ</Link>
             </div>
         </div>
       </div>
