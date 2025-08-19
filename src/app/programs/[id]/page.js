fde2ebe7acd778ad8bbe24918a039d9d579286a8
sokundeny <sokundeny18@gmail.com>
2025-08-19 15:23:25 +0700

    complete faq

--- src/app/programs/[id]/page.js
+++ src/app/programs/[id]/page.js
@@ -5,12 +5,15 @@ import KeyBenefits from "@/app/programs/[id]/_components/KeyBenefits"
 import PROGRAM_OVERVIEW from "@/data/program_overview"
 import PROGRAM_COURSES_CONTENT from "@/data/program_courses"
 
+import Program_faq from "./_components/programs_faq"
+
 const ProgramDetailPage = () => {
   return(
     <main>
       <ProgramOverview data={PROGRAM_OVERVIEW}/>
       <ProgramCourses courses={PROGRAM_COURSES_CONTENT}/>
       <KeyBenefits />
+      <Program_faq />
     </main>
   )
 }
