944486e4178657cfa1fd4d69be316a39ed3e6809
methy <methymuy@gmail.com>
2025-09-01 09:55:01 +0700

    program

--- src/app/courselist/page.jsx
+++ src/app/courselist/page.jsx
@@ -1,17 +1,60 @@
-import CourseListHeader from "./_components/CourseListHeader"
-import CourseManagement from "./_components/CourseManagement"
+"use client";
 
-import COURSE_LIST_CONTENT from "@/data/courseList-content"
-import COURSE_MANAGEMENT_CONTENT from "@/data/course_management_content"
+import { useRef, useState, useEffect } from "react";
+import CourseListHeader from "./_components/CourseListHeader";
+import CourseManagement from "./_components/CourseManagement";
 
 const CourseList = () => {
-	return(
-		<main className="mt-20">
-			
-			<CourseListHeader data={COURSE_LIST_CONTENT.HEADER}/>
-			<CourseManagement COURSE_MANAGEMENT_CONTENT={COURSE_MANAGEMENT_CONTENT}/>
-		</main>
-	)
-}
+  const categoryRefs = useRef({});
+  const [content, setContent] = useState([]);
+  const [headerData, setHeaderData] = useState({
+    totalCourses: 0,
+    totalPrograms: 0,
+    categories: [],
+  });
 
-export default CourseList
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        const res = await fetch("/api/courses");
+        if (!res.ok) {
+          throw new Error("Failed to fetch courses");
+        }
+        const data = await res.json();
+        setContent(data);
+        const totalCourses = data.reduce((sum, section) => sum + section.courses.length, 0);
+        const totalPrograms = data.length;
+        const categories = data.map((section) => ({
+          id: section.id,
+          name: section.title,
+        }));
+        
+        setHeaderData({
+          totalCourses,
+          totalPrograms,
+          categories,
+        });
+
+      } catch (error) {
+        console.error("Error fetching courses:", error);
+      }
+    };
+    fetchCourses();
+  }, []);
+
+  const handleCategoryClick = (categoryId) => {
+    const element = categoryRefs.current[categoryId];
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth", block:"end"});
+    }
+  };
+
+  return (
+    <main className="mt-20">
+      <CourseListHeader data={headerData} onCategoryClick={handleCategoryClick} />
+      <CourseManagement ref={categoryRefs} content={content} />
+    </main>
+  );
+};
+
+export default CourseList;
