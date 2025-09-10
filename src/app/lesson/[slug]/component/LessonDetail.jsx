35522fe3f740d4c72f94e23f22ed785b48bdc2bd
sokundeny <sokundeny18@gmail.com>
2025-09-10 13:50:57 +0700

    add component

---
+++ src/app/lesson/[slug]/component/LessonDetail.jsx
@@ -0,0 +1,46 @@
+"use client"
+
+import Link from "next/link";
+import RightArrow from "@/components/icons/RightArrow"
+import Tick from "@/components/icons/Tick"
+
+const LessonDetail = ({lesson,markComplete}) => {
+  return (
+      <div className="w-full">
+
+        <h1 className="text-center mx-auto text-3xl font-semibold">{lesson.order_number}. {lesson.title}</h1>
+        <p className="my-10 text-xl font-semibold">{lesson.key_takeaway_text}</p>
+
+        {lesson.lessonContents.map((content, index) => {
+          if (content.content_type === "image") {
+            return (
+              <div key={index} className="w-full">
+                <img className="w-full h-full object-contain" src={content.content} alt="lesson picture" />
+              </div>
+            );
+          } else {
+            return (
+              <p key={index} className="my-2 text-lg flex items-center gap-2">
+                {content.content_type === "list" && <span className="text-4xl flex-shrink-0">•</span>}
+                <span>{content.content}</span>
+              </p>
+            );
+          }
+        })}
+
+        <div className="flex justify-between items-center flex-row-reverse my-10 w-full">
+
+          <div
+            onClick={markComplete}
+            className={`flex items-center justify-start px-12 py-2 bg-green-800
+             text-white text-lg cursor-pointer`}
+          >
+            បញ្ចប់មេរៀន <Tick />
+          </div>
+            <Link href={`${lesson.prevSlug?`/lesson/${lesson.prevSlug}`:`/content/${lesson.courseContentSlug}`}`} className={`flex items-center justify-start px-12 py-2 bg-primary text-white text-lg cursor-pointer`}><RightArrow className={`rotate-180 h-6 w-6`}/>{lesson.prevSlug?"ចំណុចមុន":"ទៅមេរៀន"}</Link>
+          </div>
+      </div>
+  )
+}
+
+export default LessonDetail
