7ea78aa5764345d30a0d0b39cb0a2813e8fe31af
sokundeny <sokundeny18@gmail.com>
2025-09-11 16:02:58 +0700

    fix lesson

--- src/app/api/lesson/[slug]/route.js
+++ src/app/api/lesson/[slug]/route.js
@@ -5,7 +5,6 @@ export async function GET (req, { params }) {
     const { slug } = await params;
 
     const { searchParams } = new URL(req.url);
-    if (searchParams.get("admin"))
     
     try {
         const include = {
